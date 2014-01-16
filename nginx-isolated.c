5b08216deea1d607e50aae064174d1ce0cbaf07d
abhishekwy <abhishek.tiwari@webyog.com>
2014-01-16 19:19:30 +0530

    debugged

--- nginx-isolated.c
+++ nginx-isolated.c
@@ -3,6 +3,14 @@
 #include <curl/curl.h>
 #include <stdint.h>
 
+/**
+ * 1. Conditionally account for the HTTPHeader.
+ * 
+ * 
+ * 
+ **/
+
+
 static char *url_text = "http://localhost/k/nginx_status";
 //static char *user        = NULL;
 //static char *pass        = NULL;
@@ -16,8 +24,8 @@ static char *host_header = "cloudmagic.com";
 static CURL *curl = NULL;
 
 int main() {
-    char header_text[1024];
-    char total_text[1024];
+    char header_text[1024] = {0};
+    char total_text[1024] = {0};
     struct curl_slist *curl_list = NULL;
 
 
@@ -37,23 +45,17 @@ int main() {
     strncat(header_text, host_header, strlen(host_header));
     strncat(header_text, quote_text, strlen(quote_text));
 
-    printf("\n>%s<\n", url_text);
-    printf("\n>%s<\n", header_text);
-    strncat(total_text, url_text, strlen(url_text));
-    printf("\n>%s<\n", total_text);
-
-    
+    strcpy(total_text, url_text);
     strncat(total_text, header_text, strlen(header_text));
-    printf("\n>%s<\n", header_text);
-    printf("\n%s\n", total_text);
 
-    //    curl_list = curl_slist_append(curl_list, header_text);
-    //    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_list);
+    curl_list = curl_slist_append(curl_list, header_text);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_list);
 
     if (total_text != NULL) {
         curl_easy_setopt(curl, CURLOPT_URL, total_text);
     }
 
+    printf("\n\n%s\n\n", total_text);
     CURLcode res;
 
     res = curl_easy_perform(curl);
