89726b03651f8082b339822ea323b0c1d05d0748
abhishekwy <abhishek.tiwari@webyog.com>
2014-01-16 15:41:41 +0530

    curl library functions fetch

--- nginx-isolated.c
+++ nginx-isolated.c
@@ -3,7 +3,7 @@
 #include <curl/curl.h>
 #include <stdint.h>
 
-static char *url = "http://localhost/k/nginx_status";
+static char *url_text = "http://localhost/k/nginx_status";
 //static char *user        = NULL;
 //static char *pass        = NULL;
 //static char *verify_peer = NULL;
@@ -36,18 +36,29 @@ int main() {
     strcpy(header_text, " -H \"");
     strncat(header_text, http_header, strlen(http_header));
     strcat(header_text, "\"");
-    printf("\n>>%s%s<<\n", url, header_text);
-    strncat(total_text, url, strlen(url));
+
+    strncat(total_text, url_text, strlen(url_text));
     strncat(total_text, header_text, strlen(header_text));
     printf("\n%s\n", total_text);
 
-    curl_list = curl_slist_append(curl_list, total_text);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_list);
+    //    curl_list = curl_slist_append(curl_list, header_text);
+    //    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_list);
 
-    if (url != NULL) {
-        curl_easy_setopt(curl, CURLOPT_URL, url);
+    if (total_text != NULL) {
+        curl_easy_setopt(curl, CURLOPT_URL, total_text);
     }
 
+    CURLcode res;
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+
+    printf("\nret: %s\n", res);
+
+    curl_easy_cleanup(curl);
+    curl_slist_free_all(curl_list);
 
 }
 
